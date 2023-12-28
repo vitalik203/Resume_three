32820dd6f285082543082c69bd7778f4523d7570
VitaliiZaluskii <vzaluskiy23@gmail.com>
2023-12-28 15:48:25 +0200

    perf: initial commit (branch: master)
    BREAKING CHANGE: initial commit

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ['develop'],
+    branches: ['master', 'develop'],
     repositoryUrl: 'https://github.com/vitalik203/Resume-three.git',
     plugins: [
       '@semantic-release/commit-analyzer',
