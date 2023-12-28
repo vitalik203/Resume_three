b097ec718bf469bfadec3094e0ac234ac17d1504
VitaliiZaluskii <vzaluskiy23@gmail.com>
2023-12-28 19:00:34 +0200

    feat: finished

--- release.config.js
+++ release.config.js
@@ -2,16 +2,11 @@ module.exports = {
     branches: ['master', 'develop'],
     repositoryUrl: 'https://github.com/vitalik203/Resume_three.git',
     plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      {
-        // Custom configuration for @semantic-release/git plugin
-        "assets": ["CHANGELOG.md"],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      },
-      '@semantic-release/github',
-      '@semantic-release/npm',
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/npm',
+        '@semantic-release/github',
     ],
   };
   
