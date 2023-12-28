eb5e693fc95fc74b8708c155f0de629a3149d0ca
VitaliiZaluskii <vzaluskiy23@gmail.com>
2023-12-28 15:43:15 +0200

    perf: initial commit
    BREAKING CHANGE: Major (initial commit)

---
+++ release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    branches: ['develop'],
+    repositoryUrl: 'https://github.com/vitalik203/Resume-three.git',
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      {
+        // Custom configuration for @semantic-release/git plugin
+        "assets": ["CHANGELOG.md"],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      },
+      '@semantic-release/github',
+      '@semantic-release/npm',
+    ],
+  };
+  
